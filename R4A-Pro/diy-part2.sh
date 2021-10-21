f6c94ec2c0ae013a66443248bcda8ea088ede8a1
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-10-21 23:07:15 +0800

    Update diy-part2.sh

--- R4A-Pro/diy-part2.sh
+++ R4A-Pro/diy-part2.sh
@@ -42,12 +42,14 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-theme-bootstrap
 rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
+rm -rf feeds/kenzo/luci-theme-ifit
+rm -rf package/kenzo/luci-theme-ifit
 
 #下载主题luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
 #git clone https://github.com/YL2209/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
-#git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
+git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
 #git clone https://github.com/YL2209/luci-theme-edge2.git package/lean/luci-theme-edge2
 git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
 
