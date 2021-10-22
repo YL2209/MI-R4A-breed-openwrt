5739da9c2cd2fe789d8e17bba7d9425be66578e5
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-10-22 12:08:51 +0800

    Update diy-part2.sh

--- Redmi-AX6/diy-part2.sh
+++ Redmi-AX6/diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='MiWIFI'' package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-sed -i 's/OpenWrt /星新课代表 /g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt /$(TZ=UTC-8 date "+%Y.%m.%d")-星新课代表 /g' package/lean/default-settings/files/zzz-default-settings
 
 sed -i "/exit 0/i\echo '#iptables -t nat -N PrivoxyUA' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i\echo '#iptables -t nat -A PREROUTING -i br-lan -p tcp -j PrivoxyUA' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
