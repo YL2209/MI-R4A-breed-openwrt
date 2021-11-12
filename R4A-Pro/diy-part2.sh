d1bbe941e638e4283dd6dc242d5441ce07219b3c
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-11-13 00:09:29 +0800

    Update diy-part2.sh

--- R4A-Pro/diy-part2.sh
+++ R4A-Pro/diy-part2.sh
@@ -12,18 +12,13 @@
 
 # Modify default IP
 # 修改openwrt登陆地址,把下面的192.168.31.1修改成你想要的就可以了
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把XXKDB-R4A修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='MiWIFI'' package/lean/default-settings/files/zzz-default-settings
 
-sed -i "/exit 0/i\echo '#iptables -t nat -N PrivoxyUA' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\echo '#iptables -t nat -A PREROUTING -i br-lan -p tcp -j PrivoxyUA' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\echo '#iptables -t nat -A PrivoxyUA -d 192.168.0.0/16 -j RETURN' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\echo '#iptables -t nat -A PrivoxyUA -p tcp --dport 80 -j REDIRECT --to-ports 8118' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\echo '#iptables -t nat -A PrivoxyUA -p tcp --dport 443 -j REDIRECT --to-ports 8118' >> /etc/firewall.user" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\ " package/lean/default-settings/files/zzz-default-settings
-
+target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
+echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> target/linux/$target/config*
 
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 #sed -i "s/OpenWrt /ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
@@ -136,5 +131,4 @@ sed -i 's/ssid=OpenWrt/ssid=MIWIFI_2021/g' package/kernel/mac80211/files/lib/wif
 # 修改默认wifi密码key为password
 sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #使用sed 在第四行后添加新字
-#sed -e 120a\set wireless.default_radio${devidx}.key=XXKDB-R4A package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
