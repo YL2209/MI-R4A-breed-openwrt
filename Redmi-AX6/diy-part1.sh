992dcff8eafcd5561c771d9e7f0b48d2144ee0f3
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-11-27 00:28:48 +0800

    Update diy-part1.sh

--- Redmi-AX6/diy-part1.sh
+++ Redmi-AX6/diy-part1.sh
@@ -22,5 +22,4 @@
 # passwall依赖
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-git clone https://github.com/Zxilly/UA2F package/UA2F
+
