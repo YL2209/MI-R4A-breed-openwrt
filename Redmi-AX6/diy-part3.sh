9cb52db0532f0829c08ddc831bf326f6964b58d0
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-11-27 00:45:04 +0800

    Update diy-part3.sh

--- Redmi-AX6/diy-part3.sh
+++ Redmi-AX6/diy-part3.sh
@@ -1,4 +1,16 @@
-  mv Redmi-AX6/bg/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (Before Update feeds)
+#
+
+mv Redmi-AX6/bg/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
   mv Redmi-AX6/bg/logo.png openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/logo.png
   mv Redmi-AX6/bg/bg1.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg1.jpg
   mv Redmi-AX6/bg/bg2.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg2.jpg
