4ac119696766dcc273d2d779d46081d333d748a5
YL2209 <59274897+YL2209@users.noreply.github.com>
2021-11-27 00:45:51 +0800

    Update diy-part3.sh

--- Redmi-AX6/diy-part3.sh
+++ Redmi-AX6/diy-part3.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 3 (Before Update feeds)
 #
 
-mv Redmi-AX6/bg/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
+  mv Redmi-AX6/bg/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
   mv Redmi-AX6/bg/logo.png openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/logo.png
   mv Redmi-AX6/bg/bg1.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg1.jpg
   mv Redmi-AX6/bg/bg2.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg2.jpg
