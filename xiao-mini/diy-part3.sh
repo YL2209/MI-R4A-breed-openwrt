a199f372b24f633319aebdc63ab9bb89ea44fe8d
YL2209 <59274897+YL2209@users.noreply.github.com>
2022-07-06 22:03:01 +0800

    Create diy-part3.sh

---
+++ xiao-mini/diy-part3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (Before Update feeds)
+#
+
+mv R4A-Pro/bg/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
+mv R4A-Pro/bg/logo.png openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/logo.png
+mv R4A-Pro/bg/bg1.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg1.jpg
+mv R4A-Pro/bg/bg2.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg2.jpg
+mv R4A-Pro/bg/bg3.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg3.jpg
+mv R4A-Pro/favicon.ico openwrt/package/lean/luci-theme-ifit/files/htdocs/favicon.ico
+mv R4A-Pro/logo.png openwrt/package/lean/luci-theme-ifit/files/htdocs/logo.png      
+mv R4A-Pro/bg/main_bg.jpg openwrt/package/lean/luci-theme-ifit/files/htdocs/images/main_bg.jpg
+mv R4A-Pro/bg/main_bg_5k.jpg openwrt/package/lean/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg  
+mv R4A-Pro/banner openwrt/package/base-files/files/etc/banner
